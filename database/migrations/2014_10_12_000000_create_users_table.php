9f14f376e17f11ffd03cd049333f3ed238732337
AmirFayaz <Amirfayazfanavar>
2022-10-07 12:25:27 +0330

    fix: make phone nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('phone');
+            $table->integer('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
