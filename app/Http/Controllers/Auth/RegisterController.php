80dd0b7c258834768a1eeb4629b067505846b79d
AmirFayaz <Amirfayazfanavar>
2022-10-08 09:51:30 +0330

    feat: finish create user method

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Requests\User\RegisterRequest;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 
 class RegisterController extends Controller
@@ -12,7 +12,8 @@ class RegisterController extends Controller
 
     public function register(RegisterRequest $request){
 
-        User::create($request->validated());
-        return response('User Registered Succesfully',201);
+        $user = User::create($request->validated());
+        $token = $user->createToken('user_token')->plainTextToken;
+        return response(['user' => $user, 'token' => $token],201);
     }
 }
