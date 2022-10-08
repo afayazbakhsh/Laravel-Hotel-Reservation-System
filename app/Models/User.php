764da1b17addfc3a46eb4456d4042c5ef13b8c0c
AmirFayaz <Amirfayazfanavar>
2022-10-08 09:47:24 +0330

    feat: add setter method for hash password

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($value){
+
+        $this->attributes['password'] = bcrypt($value);
+    }
+
 }
