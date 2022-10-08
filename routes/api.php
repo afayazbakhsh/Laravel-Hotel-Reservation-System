40616924f5e3a222eb864821456380e43d99aca8
AmirFayaz <Amirfayazfanavar>
2022-10-08 09:55:10 +0330

    feat: all route for authenticate

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\User\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,12 +17,24 @@ use App\Http\Controllers\Auth\RegisterController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::prefix('v1')->group(function(){
+
+    //Auth Route
     Route::prefix('auth')->group(function(){
+        //Register
         Route::post('register',[RegisterController::class,'register']);
+        //Login
+        Route::post('login',[LoginController::class,'login']);
+        //Logout
+        Route::middleware('auth:sanctum')->get('logout',[LogoutController::class,'logout']);
+    });
+
+    //Protected Route User
+    Route::middleware('auth:sanctum')->prefix('users')->group(function(){
+        Route::resource('/',UserController::class);
     });
 });
