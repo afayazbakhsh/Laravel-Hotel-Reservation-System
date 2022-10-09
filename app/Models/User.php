d32aa3e8f0dd8727125bfea7cb5dbdeb7e620012
AmirFayaz <Amirfayazfanavar>
2022-10-09 10:33:21 +0330

    feat: make bcrypt for password attribute

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
@@ -41,9 +42,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function setPasswordAttribute($value){
 
-        $this->attributes['password'] = bcrypt($value);
+    protected function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => bcrypt($value),
+        );
     }
 
 }
