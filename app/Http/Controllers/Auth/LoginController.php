7a829968da0eae246fc7666a25206fc706005fdd
AmirFayaz <Amirfayazfanavar>
2022-10-09 10:31:05 +0330

    feat: implemented repository pattern

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,21 +5,30 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Auth\LoginRequest;
-use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Repositories\UserRepository;
+use App\Interfaces\UserRepositoryInterface;
+use Illuminate\Http\JsonResponse;
 
 class LoginController extends Controller
 {
+    private UserRepositoryInterface $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository){
+
+        $this->userRepository = $userRepository;
+    }
+
     public function login(LoginRequest $request){
 
-        //check email
-        $user = User::where('email',$request->email)->first();
+        //check user
+        $user = $this->userRepository->getUserByEmail($request->email);
         //check password
         if(!$user || !Hash::check($request->password,$user->password)){
-            return response(['message'   =>  'اطلاعات وارد شده اشتباه میباشد',],401);
+            return response(['message'   =>  'اطلاعات وارد شده اشتباه میباشد'],401);
         }
         //create token
-        $token = $user->createToken('user_token')->plainTextToken;
-        return response(['user' => $user,'token' => $token], 200);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response(['user' => $user,'access_token' => $token], 200);
     }
 }
