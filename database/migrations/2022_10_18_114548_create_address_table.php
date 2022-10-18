5f97f722be6a322f425730d3e76c4824f0d239e1
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-10-18 09:11:55 -0400

    build: create address table

---
+++ database/migrations/2022_10_18_114548_create_address_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('addressable');
+            $table->string('address');
+            $table->double('lat')->nullable();
+            $table->double('lng')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+};
