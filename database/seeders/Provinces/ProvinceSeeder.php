4cfc01dac515f0eaed9826477356ca675bdc6fdd
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-10-20 13:05:48 +0330

    feat: store province data from excel to database

--- database/seeders/Provinces/ProvinceSeeder.php
+++ database/seeders/Provinces/ProvinceSeeder.php
@@ -2,11 +2,10 @@
 
 namespace Database\Seeders\Provinces;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Log;
 use App\Models\Province;
-use App\Classes\Excels\Convert;
+use App\Interfaces\ConvertServiceInterface;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class ProvinceSeeder extends Seeder
@@ -16,20 +15,26 @@ class ProvinceSeeder extends Seeder
      *
      * @return void
      */
+
+    public function __construct(ConvertServiceInterface $service)
+    {
+        $this->convertService = $service;
+    }
+
     public function run()
     {
 
         if (Storage::disk('public')->exists('excels/province.xlsx')) {
 
             //convert excels data to array
-            $provinces = Convert::convertToArray('excels/province.xlsx');
+            $provinces = $this->convertService->ExcelToArray('excels/province.xlsx');
 
             foreach ($provinces as $data) {
 
                 $province = collect(['name' => $data[0], 'latitude' => $data[2], 'longitude' => $data[3]]);
 
                 Province::create([
-                    'name' =>   $province['name'],
+                    'name' =>   $this->convertService->ArabicWordToPersian($province['name']),
                     'latitude' =>   $province['latitude'],
                     'longitude' =>   $province['longitude'],
                 ]);
