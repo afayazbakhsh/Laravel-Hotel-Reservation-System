9460e862be4cf89164259810849c9d4ee0e4cd3f
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-10-20 13:15:00 +0330

    build: create province entity

--- database/migrations/2022_10_18_113446_create_hotels_table.php
+++ database/migrations/2022_10_18_113446_create_hotels_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('hotels', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
             $table->string('name');
             $table->timestamps();
             $table->softDeletes();
