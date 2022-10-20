e0c801b676f636dfd28a9bb638fa6857373404c9
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-10-20 13:13:28 +0330

    feat: define convert service in register method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\ConvertServiceInterface;
+use App\Services\ConvertService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(ConvertServiceInterface::class, ConvertService::class);
     }
 
     /**
