2ed8859d60d4759ed9b1aeb87196eebc1c84b443
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-10-27 10:44:49 +0330

    feat: city hotels relation method

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+
+
+    public function hotels()
+    {
+        return $this->hasMany(Hotel::class);
+    }
 }
