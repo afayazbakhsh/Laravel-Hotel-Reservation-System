23d776f71d37c91297e672006f93aef99c88d5a6
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-11-09 10:22:48 +0330

    feat: roomType relation with rooms

---
+++ app/Models/RoomType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RoomType extends Model
+{
+    use HasFactory;
+
+
+
+    public function rooms(){
+
+        return $this->hasMany(Room::class);
+    }
+}
