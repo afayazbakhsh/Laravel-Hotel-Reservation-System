87ffe71388b0fa397ce867ac4eafe43fcdac0355
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-11-19 16:40:02 +0330

    feat: finish index method

--- app/Http/Controllers/Host/HostController.php
+++ app/Http/Controllers/Host/HostController.php
@@ -4,13 +4,32 @@ namespace App\Http\Controllers\Host;
 
 use App\Http\Controllers\Controller;
 use App\Models\Host;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class HostController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        // get hosts who has a hotel in special city
+        return Host::where(function ($query) use ($request){
 
-        return Host::confirmed()->with(['hotel'])->get();
+            if($request->has('national_code')){
+
+                $query->where('national_code',$request->national_code);
+            }
+
+        })->whereHas('hotel', function ($query) use ($request) {
+
+                if ($request->has('confirmed')) {
+
+                    $query->confirmed($request->confirmed);
+                }
+
+                if ($request->has('city_id')) {
+
+                    $query->where('city_id', $request->city_id);
+                }
+            })->confirmed()->latest()->get();
     }
 }
