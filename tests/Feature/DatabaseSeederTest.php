12e281764f139364285a4004df31998e6e4c1af2
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2023-01-23 19:43:06 +0330

    feat: test database after run seeders

---
+++ tests/Feature/DatabaseSeederTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DatabaseSeederTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_seeder_can_created(){
+        $this->seed();
+        $this->assertDatabaseCount('hosts', 100);
+        $this->assertDatabaseCount('hotels', 100);
+    }
+}
