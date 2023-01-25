dab481133a9afee323f2c90c941c0735677115f6
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2023-01-25 10:46:21 +0330

    refactor: remove cache class from this query

--- app/Http/Controllers/Host/HostController.php
+++ app/Http/Controllers/Host/HostController.php
@@ -3,39 +3,35 @@
 namespace App\Http\Controllers\Host;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\HostCollection;
 use App\Http\Resources\HostResource;
 use App\Models\Host;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 
 class HostController extends Controller
 {
     public function index(Request $request)
     {
         // get hosts who has a hotel
-        $hosts = Cache::remember('hosts', 60 * 60, function () use ($request) {
+        $hosts = Host::where(function ($query) use ($request) {
+            // Get data with national code
+            $query->when(request('national_code'), function ($query) use ($request) {
 
-            return Host::where(function ($query) use ($request) {
+                $query->where('national_code', $request->national_code);
+            });
+        })->whereHas('hotel', function ($query) use ($request) {
 
-                $query->when(request('national_code'), function ($query) use ($request) {
+            if ($request->has('confirmed')) {
 
-                    $query->where('national_code', $request->national_code);
-                });
-            })->whereHas('hotel', function ($query) use ($request) {
+                $query->confirmed($request->confirmed);
+            }
 
-                if ($request->has('confirmed')) {
+            if ($request->has('city_id')) {
 
-                    $query->confirmed($request->confirmed);
-                }
+                $query->where('city_id', $request->city_id);
+            }
+        })->with('hotel.city', 'hotel.address')->confirmed()->latest()->get();
 
-                if ($request->has('city_id')) {
-
-                    $query->where('city_id', $request->city_id);
-                }
-            })->with('hotel.city', 'hotel.address')->confirmed()->latest()->get();
-        });
         return HostResource::collection($hosts);
     }
 }
