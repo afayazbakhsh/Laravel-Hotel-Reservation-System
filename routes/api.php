a54478f0ec71c1a1a5b92e802bc3816de146e2ca
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2023-02-11 12:20:41 +0330

    refactor: use apiResource method for hotel crud operation

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Host\HostController;
 use App\Http\Controllers\Hotel\HotelController;
-use App\Http\Controllers\Registration\HotelRegistrationController;
-use App\Http\Controllers\User\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,22 +32,6 @@ Route::prefix('v1')->group(function () {
         Route::middleware('auth:sanctum')->get('logout', [LogoutController::class, 'logout']);
     });
 
-    //User Routes
-    Route::resource('users', UserController::class);
-    //Host Routes
-    Route::resource('hosts', HostController::class);
-    //Hotel Registeration request
-    Route::post('hotel-registeration', HotelRegistrationController::class);
-
-    // Hotel Routes
-    Route::prefix('/')->group(function () {
-
-        Route::get('hotels/', [HotelController::class, 'index']);
-
-        Route::post('hosts/{host}/hotels', [HotelController::class, 'store']);
-
-        Route::get('hosts/{host}/hotels/{hotel}', [HotelController::class, 'show']);
-
-        Route::put('hosts/{host}/hotels/{hotel}', [HotelController::class, 'update']);
-    });
+    // Crud operation
+    Route::apiResource('hotels',HotelController::class);
 });
