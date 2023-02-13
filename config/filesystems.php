845b94269b1fe4dc76c74751297b9682092f8d93
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2023-02-13 16:32:07 +0330

    fix: add public visibility to s3 disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,6 +54,7 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', true),
             'throw' => false,
+            'visibility' => 'public',
         ],
 
         'media' => [
