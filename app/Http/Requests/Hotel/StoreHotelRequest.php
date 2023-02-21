c6fc1f15e92c9179d23e295c718e38eeb9d81b36
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2023-02-21 07:47:55 +0330

    refactor: make rules for storing validate

--- app/Http/Requests/Hotel/StoreHotelRequest.php
+++ app/Http/Requests/Hotel/StoreHotelRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests\Hotel;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 class StoreHotelRequest extends FormRequest
 {
@@ -24,8 +26,31 @@ class StoreHotelRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'      => 'required|max:300',
-            'city_id'   => 'required|exists:cities,id',
+            'name'          => 'required|max:300',
+            'title'         => 'required|min:2',
+            'city_id'       => 'required|exists:cities,id',
+            'host_id'       => 'required|exists:hosts,id',
+            'description'   => 'required|max:2000',
+            'motto'          => "required|min:5",
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+
+
+    public function messages()
+    {
+        return [
+            'required'          => 'تمامی فیلد هارا پر کنید',
+            'name.max'          => 'نام بیش از حد طولانی است',
+            'city_id.exists'    => 'این شهر وجود ندارد',
         ];
     }
 }
