c5afd25d150ad741a2db4416d165a9474fda694f
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2023-02-23 15:03:10 +0330

    feat: add sanctum abilities middleware class to kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,8 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        // Laravel sanctum abilities
+        'abilities' => \Laravel\Sanctum\Http\Middleware\CheckAbilities::class,
+        'ability' => \Laravel\Sanctum\Http\Middleware\CheckForAnyAbility::class,
     ];
 }
